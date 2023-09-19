70175953bc1d575ebb380737041b9e682d2c994a
Matin Zareinik <young pacino>
2023-09-19 14:48:55 +0330

    timer-service added to project

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from subprocess import check_output
 import itertools
 import config
 import string
+from datetime import datetime
 
 # this is Password Found Flag
 password_found = False
@@ -12,6 +13,7 @@ password_list = string.ascii_letters + string.digits + r'!@#$%+/_*'
 
 # range(Minimum Digit, Maximum Digit)
 # Example --> Minimum Digit (2 --> a,b,c,...,*) | Maximum Digit (5 --> aaaaa,...,*_/+%)
+start_time = datetime.now()
 for item in range(1, 17):
     generator = itertools.product(password_list, repeat=item)
 
@@ -27,3 +29,8 @@ for item in range(1, 17):
             continue
     if password_found is True:
         break
+
+end_time = datetime.now()
+
+duration = end_time - start_time
+print(f'duration: {duration}')
