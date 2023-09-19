041894b80e2c99124204b847d371f752bcb453c9
Matin Zareinik <young pacino>
2023-09-19 14:37:53 +0330

    main.py created

---
+++ main.py
@@ -0,0 +1,29 @@
+from subprocess import check_output
+import itertools
+import config
+import string
+
+# this is Password Found Flag
+password_found = False
+
+# Error Characters "&\'(),-.:;<=>?[\\]^`{|}~
+password_list = string.ascii_letters + string.digits + r'!@#$%+/_*'
+
+
+# range(Minimum Digit, Maximum Digit)
+# Example --> Minimum Digit (2 --> a,b,c,...,*) | Maximum Digit (5 --> aaaaa,...,*_/+%)
+for item in range(1, 17):
+    generator = itertools.product(password_list, repeat=item)
+
+    for password in generator:
+        try:
+            # check Password File by check_output
+            check_output(fr'"{config.UNRAR_FILE_ADRESS}" x test.rar -p' + ''.join(password), shell=True)
+            print(f"password find! --> {''.join(password)}")
+            password_found = True
+            break
+        except:
+            print(f" --> {''.join(password)}")
+            continue
+    if password_found is True:
+        break
